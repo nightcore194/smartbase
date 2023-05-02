925965d207833ae1c7a4e17fef4a85071149439c
300nn <slobodchikov2003@gmail.com>
2023-05-02 14:15:35 +0500

    init commit
    добавлено vue приложение
    реализована сортировка таблицы

---
+++ web/client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex flex-column h-full">
+        <div class="border flex-grow-1 flex">
+            <TableSIOM :color="'green'" class="flex-grow-1 scroll"></TableSIOM>
+            <TableVK :color="'yellow'" class="flex-grow-1 scroll"></TableVK>
+        </div>
+        <div class="border flex-grow-1">
+            <UserConfigurate></UserConfigurate>
+        </div>
+        <div class="border flex-grow-1">
+            <TabelPrognos :color="'red'"></TabelPrognos>
+        </div>
+    </div>
+</template>
+
+<script  lang="ts">
+import { defineComponent } from 'vue'
+import TableSIOM from './components/TableSIOM.vue'
+import TableVK from './components/TableVK.vue'
+import TabelPrognos from './components/TabelPrognos.vue'
+import UserConfigurate from './components/UserConfigurate.vue'
+export default defineComponent({
+    components: { TableSIOM, TableVK, TabelPrognos ,UserConfigurate },
+    mounted() {
+
+    }
+})
+</script>
+
+<style scoped></style>
---
+++ web/client/src/components/UserConfigurate.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="flex  align-items-center justify-content-center h-full controls mt-1 mb-3">
+        <div>
+            <div class="flex align-items-center justify-content-center h-full controls mt-1 mb-2">
+                <Button class='mr-2 w-15rem' label="Расчитать" severity="success" />
+                <Dropdown v-model="select1" :options="cities" optionLabel="name" placeholder="№ Сиом"
+                    class="w-full md:w-14rem" />
+                <Dropdown v-model="select2" :options="cities" optionLabel="name" placeholder="№ ВК"
+                    class="w-full md:w-14rem" />
+                <InputText v-model="inputText" :disabled="true"></InputText>
+
+                {{ dataStore.rows.length }}
+            </div>
+            <div>
+                <Button class="w-15rem" label="Использовать" severity="success"/>
+            </div>
+        </div>
+    </div>
+
+</template>
+  
+<script  lang="ts">
+import { useDataStore } from '@/stores/data';
+import { mapState, mapStores } from 'pinia';
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    computed: {
+        ...mapStores(useDataStore)
+    },
+    setup() {
+        return {
+            cities: [
+                { name: 'New York', code: 'NY' },
+                { name: 'Rome', code: 'RM' },
+                { name: 'London', code: 'LDN' },
+                { name: 'Istanbul', code: 'IST' },
+                { name: 'Paris', code: 'PRS' }
+            ],
+
+        }
+    },
+    data() {
+        return {
+            select1: null,
+            select2: null,
+            inputText: 'Класс ЭЧ'
+        }
+    },
+    mounted() {
+        this.dataStore.getData();
+    }
+})
+</script>
+  
+<style scoped>
+.controls *+* {
+    margin-right: 0.5rem
+}
+</style>
+  
